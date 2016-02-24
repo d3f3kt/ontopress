7a917c563d9fa72e35f4f725212dd5475f63c4b1
David Geistert <d3f3kt@scripts4u.net>
2016-02-24 17:11:06 +0000

    add: dummy config and dummy controller

---
+++ src/OntoPress/Controller/TestController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace OntoPress\Controller;
+
+class TestController
+{
+    public function __construct($container)
+    {
+    }
+
+    public function showTestAction()
+    {
+        return "hey";
+    }
+}
