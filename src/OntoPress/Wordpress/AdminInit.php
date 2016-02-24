d07fcbec6216899dc21a61b80a81ce66be1de6e0
David Geistert <d3f3kt@scripts4u.net>
2016-02-24 17:07:48 +0000

    del: todo comments

--- src/OntoPress/Wordpress/AdminInit.php
+++ src/OntoPress/Wordpress/AdminInit.php
@@ -3,6 +3,7 @@
 namespace OntoPress\Wordpress;
 
 use Symfony\Component\DependencyInjection\Container;
+use OntoPress\Libary\Router;
 
 class AdminInit
 {
@@ -16,17 +17,15 @@ class AdminInit
 
     private function __construct(Container $container)
     {
+        $this->container = $container;
         add_action('admin_menu', array($this, 'adminMenu'));
         add_action('admin_enqueue_scripts', array($this, 'loadResources'));
     }
 
     public function adminMenu()
     {
-        // TODO: Routing config
-        add_menu_page('Lorum Impus', 'OntoPress', 'manage_options', 'Ontopress', array('BKeys_Admin', 'displayManagePage'), 'dashicons-list-view', 30);
-        add_submenu_page('Ontopress', 'Ontologien verwalten', 'Verwalten', 'manage_options', 'Ontopress', array('ROUTING', 'displayManagePage'));
-        add_submenu_page('Ontopress', 'Kampagne hinzufügen', 'Neu hinzufügen', 'manage_options', 'OntopressAdd', array('ROUTING', 'displayAddPage'));
-        add_submenu_page(null, 'Ontologie bearbeiten', 'Bearbeiten', 'manage_options', 'OntopressOntologyEdit', array('ROUTING', 'displayEditPage'));
+        $router = new Router($this->container);
+        $router->setRoutes();
     }
 
     public function loadResources()
