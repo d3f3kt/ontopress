c6d4a7c293220d51e8a6ff0d56aa62de5d4f3c27
Christofer Meinecke <Christofer.M@gmx.de>
2016-02-24 17:36:38 +0000

    upd: documentation

--- src/OntoPress/Libary/RouterConfig.php
+++ src/OntoPress/Libary/RouterConfig.php
@@ -5,40 +5,40 @@ namespace OntoPress\Libary;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 
+/**
+ * RouterConfig used by the Router Class and defines a configuration tree to handle the router configuration
+ */
 class RouterConfig implements ConfigurationInterface
 {
+    /**
+     * {@inheritdoc}
+     */
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
         $rootNode = $treeBuilder->root('routing')
             ->children()
                 ->arrayNode('sites')
-                    ->useAttributeAsKey('name')
-                    ->prototype('array')
-                        ->children()
-                            ->scalarNode('page_title')->end()
-                            ->scalarNode('menu_title')->end()
-                            ->scalarNode('capability')->end()
-                            ->scalarNode('icon')->end()
-                            ->integerNode('position')->end()
-                            ->scalarNode('controller')->end()
-                            ->arrayNode('sub_sites')
-                                ->useAttributeAsKey('name')
-                                ->prototype('array')
-                                    ->children()
-                                        ->scalarNode('page_title')->end()
-                                        ->scalarNode('menu_title')->end()
-                                        ->scalarNode('capability')->end()
-                                        ->scalarNode('controller')->end()
-                                    ->end()
-                                ->end()
+                    ->children()
+                        ->scalarNode('page_title')->end()
+                        ->scalarNode('menu_title')->end()
+                        ->scalarNode('capability')->end()
+                        ->scalarNode('slug')->end()
+                        ->scalarNode('icon')->end()
+                        ->integerNode('position')->end()
+                        ->arrayNode('sub_sites')
+                            ->children()
+                                ->scalarNode('parent')->end()
+                                ->scalarNode('page_title')->end()
+                                ->scalarNode('menu_title')->end()
+                                ->scalarNode('capability')->end()
+                                ->scalarNode('slug')->end()
                             ->end()
                         ->end()
                     ->end()
                 ->end()
             ->end()
         ;
-
         return $treeBuilder;
     }
 }
