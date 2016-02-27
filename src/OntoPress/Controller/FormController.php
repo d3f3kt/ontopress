f70c174b42891e5f31e9a609ad016824b5159e60
Lukas Janik <Lord-kroak1@web.de>
2016-02-27 18:01:07 +0000

    Fix: space fix in formController

--- src/OntoPress/Controller/FormController.php
+++ src/OntoPress/Controller/FormController.php
@@ -7,6 +7,6 @@ class FormController extends Controller
 {
     public function showManageAction()
     {
-        return $this->render('form/manageForms.html.twig',array());
+        return $this->render('form/manageForms.html.twig', array());
     }
 }
