4853e5e3e590025ee9274fbe1e4b8b55c2bd41bb
Lukas Janik <Lord-kroak1@web.de>
2016-02-29 21:16:37 +0000

    Add: ontologyAddForm

--- src/OntoPress/Controller/OntologyController.php
+++ src/OntoPress/Controller/OntologyController.php
@@ -1,6 +1,8 @@
 <?php
 namespace OntoPress\Controller;
 
+use OntoPress\Form\Ontology\AddOntologyForm;
+use OntoPress\Form\Ontology\AddResourceDetailForm;
 use OntoPress\Libary\Controller;
 
 class OntologyController extends Controller
@@ -17,6 +19,10 @@ class OntologyController extends Controller
 
     public function showAddAction()
     {
-        return $this->render('ontology/ontologyAdd.html.twig', array());
+        $form = $this->createForm(new AddOntologyForm());
+
+        return $this->render('ontology/ontologyAdd.html.twig', array(
+            'form' => $form->createView(),
+        ));
     }
 }
