4bc9ccd165e3678d26581cfe68ace6015fa6f456
Lukas Rosenkranz <lukas.rosenkranz@gmx.de>
2016-03-06 20:47:56 +0000

    upd: Tables

--- src/OntoPress/Controller/DashboardController.php
+++ src/OntoPress/Controller/DashboardController.php
@@ -7,6 +7,19 @@ class DashboardController extends Controller
 {
     public function showDashboardAction()
     {
-        return $this->render('dashboard/dashboard.html.twig', array());
+
+        $resTableBuildings = array( array("id" => 2, "title" => "Uni Campus"),
+                                    array("id" => 3, "title" => "Oper Leipzig"));
+        $resTablePlaces = array(array("id" => 1, "title" => "Augustusplatz"));
+
+        $dashTableOnto = array(	array("id" => 1,"name" => "Gebäude","form" => 10,"resource" => 2),
+                                array("id" => 2,"name" => "Plätze","form" => 5,"resource" => 1),
+                                array("id" => 3,"name" => "Kirchen","form" => 8,"resource" => 0));
+
+        $dashTableForm = array(	array("id" => 1, "ontology" => "Gebäude", "formName" => "Schulen"),
+                                array("id" => 2, "ontology" => "Plätze", "formName" => "öffentliche Plätze"),
+                                array("id" => 3, "ontology" => "Kirchen", "formName" => "öffentliche Plätze"));
+        return $this->render("dashboard/dashboard.html.twig", array( "dashTableForm" => $dashTableForm, "dashTableOnto" => $dashTableOnto, "resTablePlaces" => $resTablePlaces, "resTableBuildings" => $resTableBuildings));
+        //return $this->render('dashboard/dashboard.html.twig', array());
     }
 }
