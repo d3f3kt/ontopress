dd618047aed13cb9fe65008c3d1d7341002987b5
Lukas Rosenkranz <lukas.rosenkranz@gmx.de>
2016-03-10 16:11:17 +0000

    upd: table content

--- src/OntoPress/Controller/FormController.php
+++ src/OntoPress/Controller/FormController.php
@@ -8,7 +8,15 @@ class FormController extends AbstractController
 {
     public function showManageAction()
     {
-        return $this->render('form/manageForms.html.twig', array());
+        $formManageTable = array(
+            array('id' => 1, 'name' => 'öffentliche Plätze', 'author' => 'k00ni', 'date' => '20.Jan 2016'),
+            array('id' => 2, 'name' => 'Schulen', 'author' => 'k00ni', 'date' => '20.Jan 2016'),
+            array('id' => 3, 'name' => 'Galerie', 'author' => 'd3f3ct', 'date' => '20.Jan 2016'),
+        );
+
+        return $this->render('form/manageForms.html.twig', array(
+            'formManageTable' => $formManageTable
+        ));
     }
 
     public function showEditAction()
