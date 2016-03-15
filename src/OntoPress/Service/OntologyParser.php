31c2749418ca9f726570ff24b0179f7e98d8381c
Christofer Meinecke <Christofer.M@gmx.de>
2016-03-15 16:19:11 +0000

    upd: ontology label parsing

--- src/OntoPress/Service/OntologyParser.php
+++ src/OntoPress/Service/OntologyParser.php
@@ -21,20 +21,28 @@ class OntologyParser
 
         $statementIterator = $parser->parseStringToIterator($fileContent);
 
-        echo PHP_EOL;
         //label und restriction in einem array
+        foreach ($statementIterator as $key => $predicate) {
+            //echo (string)$predicate->getPredicate();
+            if((string)$predicate->getPredicate() == "http://www.w3.org/2000/01/rdf-schema#label") {
+                echo (string)$predicate->getObject();
+                echo '<br />';
+            }
+        }
+        /*
         foreach ($statementIterator as $key => $statement) {
             echo '#' . $key . ' - ' .
                 (string)$statement->getSubject() . ' - ' .
                 (string)$statement->getPredicate() . ' - ' .
                 (string)$statement->getObject() . PHP_EOL;
             echo '<br />';
-            /*
+
+
             echo "<pre>";
             print_r($statement);
             echo '</pre><br />';
-            */
-        }
+
+        }*/
 
     }
 }
