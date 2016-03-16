ae23b54a21d72c7b72249fa2cf2be09a7cb4e8c3
Christofer Meinecke <Christofer.M@gmx.de>
2016-03-16 18:51:22 +0000

    upd: static parsing

--- src/OntoPress/Service/OntologyParser.php
+++ src/OntoPress/Service/OntologyParser.php
@@ -9,61 +9,66 @@ class OntologyParser
 {
     public function parsing($filepath)
     {
-       // $fileContent1 = file_get_contents(__DIR__.'/../Resources/ontology/knorke.ttl');
-       // $fileContent = file_get_contents(__DIR__.'/../Resources/ontology/place-ontology.ttl');
+        // $fileContent1 = file_get_contents(__DIR__.'/../Resources/ontology/knorke.ttl');
+        // $fileContent = file_get_contents(__DIR__.'/../Resources/ontology/place-ontology.ttl');
         //$fileContent = $fileContent1 . $fileContent2;
         $parser = new ParserEasyRdf(
             new NodeFactoryImpl(),
             new StatementFactoryImpl(),
             'turtle' // RDF format of the file to parse later on (ttl => turtle)
         );
-
-        //$statementIterator = $parser->parseStringToIterator($fileContent);
         $statementIterator = $parser->parseStreamToIterator($filepath);
-        //label und restriction in einem array
+
         $labelArray = array();
         $commentArray = array();
+        $restrictionArray = array();
+        $mandatoryArray = array();
         foreach ($statementIterator as $key => $statement) {
             if ((string)$statement->getPredicate() == "http://www.w3.org/2000/01/rdf-schema#label") {
                 $labelArray[(string)$statement->getSubject()] = $statement->getObject();
             } elseif ((string)$statement->getPredicate() == "http://www.w3.org/2000/01/rdf-schema#comment") {
                 $commentArray[(string)$statement->getSubject()] = $statement->getObject();
             } elseif ((string)$statement->getPredicate() == "http://localhost/k00ni/knorke/restrictionOneOf") {
-                echo (string)$statement, '<br />';
-            }
-            /*
-            if ((string)$statement->getPredicate() == "http://localhost/k00ni/knorke/isMandatory") {
-                if ((string)$statement->getObject() == "true") {
+                if ($restrictionArray[(string)$statement->getSubject()] == NULL) {
+                    $restrictionArray[(string)$statement->getSubject()] = array($statement->getObject());
                 }
-                echo '<br />';
+                else {
+                    array_push($restrictionArray[(string)$statement->getSubject()], $statement->getObject());
+                }
+            } elseif ((string)$statement->getPredicate() == "http://localhost/k00ni/knorke/isMandatory") {
+                $mandatoryArray[(string)$statement->getSubject()] = $statement->getObject();
             }
-            */
         }
-
+        /*
         echo "Labels", "<br />";
         foreach ($labelArray as $key => $object) {
             echo $key . ' - ' .
                 (string)$object;
-
             echo '<br />';
         }
         echo "Comments" , "<br />";
         foreach ($commentArray as $key => $object) {
             echo $key . ' - ' .
                 (string)$object;
-
             echo '<br />';
         }
-        $statementArray = array( "Labels" => $labelArray, "Comments" => $commentArray);
-        /*
-        foreach ($statementIterator as $key => $statement) {
-            echo '#' . $key . ' - ' .
-                (string)$statement->getSubject() . ' - ' .
-                (string)$statement->getPredicate() . ' - ' .
-                (string)$statement->getObject() . PHP_EOL;
-            echo '<br />';
+        echo "Restrictions" , "<br />";
+        foreach ($restrictionArray as $key1 => $object) {
+            foreach ($object as $key2 => $restriction) {
+                echo $key1 . ' - ' .
+                    (string)$restriction;
+                echo '<br />';
+            }
+        }
+        echo "Mandatory" , "<br />";
+        foreach ($mandatoryArray as $key => $object) {
+                echo $key . ' - ' .
+                    (string)$object;
+                echo '<br />';
         }
         */
+        $statementArray = array( "Labels" => $labelArray, "Comments" => $commentArray, "Restrictions" => $restrictionArray, "Mandatory" => $mandatoryArray);
+
         return $statementArray;
     }
 }
