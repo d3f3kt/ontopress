3b350c24543feda3caaed1fc690b4994215ed2f8
Lukas Rosenkranz <lukas.rosenkranz@gmx.de>
2016-03-16 17:50:27 +0000

    upd: Ontology upload (unfinished file move)

--- src/OntoPress/Controller/OntologyController.php
+++ src/OntoPress/Controller/OntologyController.php
@@ -34,6 +34,34 @@ class OntologyController extends AbstractController
             'cancel_link' => $this->generateRoute('ontopress_ontology'),
         ));
 
+        $form->handleRequest();
+
+        if ($form->isValid()){
+            $ontologyName = substr($ontology->getName(), 0, 3);
+            $timestamp = time();
+            $author = wp_get_current_user();
+            $date = date('j F Y');
+            $path = 'Resources/ontology/'.(string)$ontologyName.'_'.(string)$timestamp;
+            $ontology->setOntologyFile($path);
+            $ontology->setAuthor($author->user_nicename);
+            $ontology->setDate($date);
+            $this->getDoctrine()->persist($ontology);
+            $this->getDoctrine()->flush();
+
+          /*  // move takes the target directory and then the
+            // target filename to move to
+            $this->getFile()->move(
+                $this->getUploadRootDir(),
+                $this->getFile()->getClientOriginalName()
+            );
+
+            // set the path property to the filename where you've saved the file
+            $this->path = $this->getFile()->getClientOriginalName();
+
+            // clean up the file property as you won't need it anymore
+            $this->file = null; */
+        }
+
         return $this->render('ontology/ontologyAdd.html.twig', array(
             'form' => $form->createView(),
         ));
