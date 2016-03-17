1726f20d3305f47ba0a1749bd6828ec4b980c3d4
Enno <e.jertschat@gmail.com>
2016-03-17 15:16:56 +0000

    draft: new Test OntologyController

---
+++ src/OntoPress/Tests/Controller/OntologyControllerTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace OntoPress\Tests;
+
+use OntoPress\Controller\OntologyController;
+use OntoPress\Libary\OntoPressTestCase;
+
+class OntologyControllerTest extends OntoPressTestCase
+{
+    public function testShowManageAction()
+    {
+        $container = static::getContainer();
+        $ontologyController = new OntologyController($container);
+        $ontologyOutput = $ontologyController->showManageAction();
+
+        $this->assertContains("Ontologie Verwaltung", $ontologyOutput);
+    }
+
+    public function testShowDeleteAction()
+    {
+        $container = static::getContainer();
+        $ontologyController = new OntologyController($container);
+        $ontologyOutput = $ontologyController->showDeleteAction();
+
+        $this->assertContains("Ontologie Löschen", $ontologyOutput);
+    }
+
+    public function testShowAddAction()
+    {
+        $container = static::getContainer();
+        $ontologyController = new OntologyController($container);
+        $ontologyOutput = $ontologyController->showAddAction();
+
+        $this->assertContains("Ontologie hochladen", $ontologyOutput);
+    }
+
+}
