bee6c6a5ae6e03bb0dbfdbdf8f87d11c5fb3950f
Enno <e.jertschat@gmail.com>
2016-03-17 13:10:07 +0000

    upd: file moved to .../Tests/Controller

---
+++ src/OntoPress/Tests/Controller/DashboardControllerTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace OntoPress\Tests;
+
+use OntoPress\Controller\DashboardController;
+use OntoPress\Libary\OntoPressTestCase;
+
+class DashboardControllerTest extends OntoPressTestCase
+{
+    public function testDashboardController()
+    {
+        $container = static::getContainer();
+        $dashboardController = new DashboardController($container);
+        $dashboardOutput = $dashboardController->showDashboardAction();
+
+        $this->assertContains("wrap ontopressWrap", $dashboardOutput);
+    }
+}
