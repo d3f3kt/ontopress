98bd651a6083d22117e05a920e1fa4c23b723e17
Enno <e.jertschat@gmail.com>
2016-03-17 14:16:46 +0000

    udp: draft Test FromController

---
+++ src/OntoPress/Tests/Controller/FormControllerTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace OntoPress\Tests;
+
+use OntoPress\Controller\FormController;
+use OntoPress\Libary\OntoPressTestCase;
+
+class FormControllerTest extends OntoPressTestCase
+{
+    public function testShowManageAction(){
+        $container = static::getContainer();
+        $formController = new FormController($container);
+        $formOutput = $formController->showManageAction();
+
+        $this->assertContains("Formular Verwaltung", $formOutput);
+    }
+
+    public function testShowEditAction(){
+        $container = static::getContainer();
+        $formController = new FormController($container);
+        $formOutput = $formController->showEditAction();
+
+        $this->assertContains("Formular Bearbeiten", $formOutput);
+    }
+
+    public function testShowCreateAction(){
+        $container = static::getContainer();
+        $formController = new FormController($container);
+        $formOutput = $formController->showCreateAction();
+
+        $this->assertContains("Formular Anlegen", $formOutput);
+    }
+
+    public function testShowDeleteAction(){
+        $container = static::getContainer();
+        $formController = new FormController($container);
+        $formOutput = $formController->showDeleteAction();
+
+        $this->assertContains("Formular Löschen", $formOutput);
+    }
+}
