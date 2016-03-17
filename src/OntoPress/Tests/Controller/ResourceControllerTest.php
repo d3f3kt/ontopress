73a56f86af024542a310e9d73794a5b1d298d604
Enno <e.jertschat@gmail.com>
2016-03-17 15:32:32 +0000

    draft: Test ResourceController

---
+++ src/OntoPress/Tests/Controller/ResourceControllerTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace OntoPress\Tests;
+
+use OntoPress\Controller\ResourceController;
+use OntoPress\Libary\OntoPressTestCase;
+
+class ResourceControllerTest extends OntoPressTestCase
+{
+    public function testShowAddAction()
+    {
+        $container = static::getContainer();
+        $resourceController = new ResourceController($container);
+        $resourceOutput = $resourceController->showAddAction();
+
+        $this->assertContains("Ressourcen Hinzufügen", $resourceOutput);
+    }
+
+    public function testShowAddDetailsAction()
+    {
+        $container = static::getContainer();
+        $resourceController = new ResourceController($container);
+        $resourceOutput = $resourceController->showAddDetailsAction();
+
+        $this->assertContains("Ressourcen Hinzufügen", $resourceOutput);
+    }
+
+    public function testShowManagementAction()
+    {
+        $container = static::getContainer();
+        $resourceController = new ResourceController($container);
+        $resourceOutput = $resourceController->showManagementAction();
+
+        $this->assertContains("Ressourcen Verwaltung", $resourceOutput);
+    }
+
+    public function testShowDeleteAction()
+    {
+        $container = static::getContainer();
+        $resourceController = new ResourceController($container);
+        $resourceOutput = $resourceController->showDeleteAction();
+
+        $this->assertContains("Ressource Löschen", $resourceOutput);
+    }
+}
