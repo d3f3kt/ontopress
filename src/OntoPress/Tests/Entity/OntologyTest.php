6f4a1dbc18dfd74aa866a760b971434377c17c39
NoahWaLee <noahbwalle@googlemail.com>
2016-03-18 12:31:26 +0000

    draft: Test Ontology.php

---
+++ src/OntoPress/Tests/Entity/OntologyTest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace OntoPress\Tests\Entity;
+
+use OntoPress\Entity\Ontology;
+use OntoPress\Libary\OntoPressTestCase;
+
+class OntologyTest extends OntoPressTestCase
+{
+    public $ontologyTest;
+
+    public function setUp()
+    {
+        $this->ontologyTest = new Ontology();
+        $ontologyTest->setName("TestOntology");
+        $ontologyTest->setAuthor("TestAuthor");
+        $ontologyTest->setDate(01.01.2011);
+    }
+
+    public function testName()
+    {
+        $testOntology = $this->ontologyTest->getName();
+        $this->assertTrue($testOntology == "TestOntology");
+    }
+
+    public function testAuthor()
+    {
+        $testAuthor = $this->ontologyTest->getAuthor();
+        $this->assertTrue($testAuthor == "TestAuthor");
+    }
+
+    public function testDate()
+    {
+        $testDate = $this->ontologyTest->getDate();
+        $this->assertTrue($testDate == 01.01.2011);
+    }
+
+    public function testAddOntologyFile()
+    {
+
+    }
+
+    public function testRemoveOntologyFile()
+    {
+
+    }
+
+    public function testGetOntologyFiles(
+    {
+
+    }
+
+    public function testUploadFiles()
+    {
+
+    }
+    )
+}
