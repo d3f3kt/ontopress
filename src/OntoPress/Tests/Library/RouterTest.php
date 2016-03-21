1bd9b513fb81fc8f70ef2249fdbdef5f0b7bdb20
Enno <e.jertschat@gmail.com>
2016-03-21 14:51:56 +0000

    draft: Test Router.php

---
+++ src/OntoPress/Tests/Library/RouterTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace OntoPress\Tests\Library;
+
+use OntoPress\Libary\Router;
+use OntoPress\Libary\OntoPressTestCase;
+
+class RouterTest extends OntoPressTestCase
+{
+    private $router;
+
+    public function setUp()
+    {
+        //$this->router = $this->getMockBuilder('Router')->getMock();
+        $this->router = new Router(static::getContainer());
+    }
+    /*
+     * 'OntoPress\Libary\Router',
+     * array(),
+     * array(static::getContainer())
+     * */
+    public function tearDown()
+    {
+        unset($this->router);
+    }
+
+    public function testGenerate()
+    {
+        $result = $this->invokeMethod($this->router, 'generate', array("ontopress", array()));
+        $this->assertContains("?page=ontopress", $result);
+    }
+
+}
