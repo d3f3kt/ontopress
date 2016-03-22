a8c63d16df1a3891d188da6f02a6c1fc7c327c24
David Geistert <d3f3kt@scripts4u.net>
2016-03-22 21:57:59 +0000

    add: wordpress test case

---
+++ src/OntoPress/Libary/OntoPressWPTestCase.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace OntoPress\Libary;
+
+use Brain\Monkey;
+
+/**
+ * OntoPress Test case for classes which need Wordpress functions.
+ */
+class OntoPressWPTestCase extends OntoPressTestCase
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+        Monkey::setUpWP();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function tearDown()
+    {
+        Monkey::tearDownWP();
+        parent::tearDown();
+    }
+}
