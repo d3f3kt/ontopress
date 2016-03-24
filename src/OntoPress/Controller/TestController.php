d04694c840c48c2f36ca5e9958f719ef27a268c8
Dboy <dominikmichael95@gmail.com>
2016-03-24 17:49:42 +0000

    upd: moved testoutput from Parser.php

--- src/OntoPress/Controller/TestController.php
+++ src/OntoPress/Controller/TestController.php
@@ -2,14 +2,25 @@
 
 namespace OntoPress\Controller;
 
-use OntoPress\Library\AbstractController;
+use OntoPress\Libary\AbstractController;
 
 class TestController extends AbstractController
 {
     public function showAddAction()
     {
         $ontologyParser = $this->get('ontopress.ontology_parser');
-        $ontologyParser->parsing(__DIR__.'/../Resources/ontology/place-ontology.ttl');
+        $output = $ontologyParser->parsing(__DIR__.'/../Resources/ontology/place-ontology.ttl');
+        
+        foreach ($output as $key => $object) {
+            echo $object->getName() . " - " . $object->getLabel() . " - " .
+                $object->getComment() . " - " . $object->getType() . " - " .
+                $object->getMandatory() . " - ";
+            if ($object->getRestriction() != null) {
+                print_r($object->getRestriction());
+                echo "<br />";
+            }
+            echo "<br />";
+        }
         return null;
     }
 }
