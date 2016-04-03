62b2a2a4de24776b5502f0a1e525d9dd2e696255
David Geistert <d3f3kt@scripts4u.net>
2016-04-03 01:33:25 +0000

    fix: comments of twig generator

--- src/OntoPress/Service/TwigGenerator.php
+++ src/OntoPress/Service/TwigGenerator.php
@@ -1,44 +1,46 @@
 <?php
 
-namespace OntePress\Service;
+namespace OntoPress\Service;
+
+use OntoPressEntity\Form;
+use OntoPress\Entity\OntologyField;
 
 /**
- * Creates Twigcode for all selected Formfields
- *
- * Class TwigGenerator
- * @package OntePress\Service
+ * To make it more comfortable to customize forms, this class generates a base Twig
+ * structure which contains all fields of a given OntoPressForm.
  */
 class TwigGenerator
 {
     /**
-     * Creates Fieldname, that is needed in the creation function.
+     * Generate a Twig structure of an OntoPressForm and return it as string.
      *
-     * @param $formField
+     * @param Form $form
+     *
+     * @return string
+     */
+    public function creation(Form $form)
+    {
+        $twigCode = '';
+        $formFields = $form->getFormFields();
+        foreach ($formFields as $formField) {
+            $fieldName = createFieldName($formField);
+            $twigCode .= '{{form_label(form.'.$fieldName.")}}\n"
+                .'{{form_errors(form.'.$fieldName.")}}\n"
+                .'{{form_widget(form.'.$fieldName.")}}\n\n";
+        }
+
+        return $twigCode;
+    }
+
+    /**
+     * Helper function to generate unique name from ontology field object.
+     *
+     * @param OntologyField $formField
+     *
      * @return string
      */
     private function createFieldName($formField)
     {
-        $name = $formField->getForm()->getOntology()->getName() ."_". $formField->getFieldUri();
-        return $name;
-    }
-
-    /**
-     * git status
-     *
-     * @param $formEntity
-     * @return string
-     */
-    public function creation($formEntity)
-    {
-        $twigCode = "";
-        $formFields = $formEntity->getFormFields();
-        foreach ($formFields as $formField) {
-            $fieldName = createFieldName($formField);
-            $twigCode .= "{{form_label(form." . $fieldName . ")}}"
-                . "{{form_errors(form." . $fieldName . ")}}"
-                . "{{form_widget(form." .$fieldName . ")}}";
-        }
-
-        return $twigCode;
+        return $formField->getForm()->getOntology()->getName().'_'.$formField->getFieldUri();
     }
 }
