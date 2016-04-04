7d03001503bed5d46a4874893d3b5a1aebd2e188
Enno <e.jertschat@gmail.com>
2016-04-04 12:53:43 +0000

    upd: added comments, new var

--- src/OntoPress/Entity/DataOntology.php
+++ src/OntoPress/Entity/DataOntology.php
@@ -35,6 +35,14 @@ class DataOntology
     protected $ontology;
 
     /**
+     * @var String
+     * @ORM\Column(name="name", type="string", length=128)
+     * @Assert\NotBlank()
+     * @Assert\Length(min=3)
+     */
+    protected $name;
+
+    /**
      * DataOntology constructor.
      */
     public function __construct()
@@ -42,11 +50,20 @@ class DataOntology
         $this->ontologyFields = new \Doctrine\Common\Collections\ArrayCollection();
     }
 
+    /**
+     * Getter id.
+     * @return int
+     */
     public function getId()
     {
         return $this->id;
     }
 
+    /**
+     * Setter ontology.
+     * @param Ontology|null $ontology
+     * @return DataOntology
+     */
     public function setOntology(\OntoPress\Entity\Ontology $ontology = null)
     {
         $this->ontology = $ontology;
@@ -54,24 +71,65 @@ class DataOntology
         return $this;
     }
 
+    /**
+     * Getter ontology.
+     * @return Ontology
+     */
     public function getOntology()
     {
         return $this->ontology;
     }
 
+    /**
+     * Getter ontologyFields.
+     * @return \Doctrine\Common\Collections\ArrayCollection|OntologyField
+     */
     public function getOntologyFields()
     {
         return $this->ontologyFields;
     }
 
+    /**
+     * Adds an OntologyField to the array ontologyFields.
+     * @param OntologyField $newOntologyField
+     * @return DataOntology
+     */
     public function addOntologyField(\OntoPress\Entity\OntologyField $newOntologyField)
     {
         $newOntologyField->setDataOntology($this);
         $this->ontologyFields[] = $newOntologyField;
+
+        return $this;
     }
 
+    /**
+     * Removes an ontologyField from ontologyFields.
+     * @param \OntoPress\Entity\OntologyField $ontologyField
+     * @return bool TRUE if this collection contained the specified element, FALSE otherwise.
+     */
     public function removeOntologyField(\OntoPress\Entity\OntologyField $ontologyField)
     {
         return $this->ontologyFields->removeElement($ontologyField);
+    }
+
+    /**
+     * Getter name.
+     * @return String
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Setter name.
+     * @param String $newName
+     * @return $this
+     */
+    public function setName($newName)
+    {
+        $this->name = $newName;
+
+        return $this;
     }
 }
