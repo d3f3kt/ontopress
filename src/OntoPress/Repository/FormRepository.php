eb28847375e195894f12849bc1719c8724132b35
Lukas Janik <Lord-kroak1@web.de>
2016-04-05 17:06:03 +0000

    Upd: Repository comments

--- src/OntoPress/Repository/FormRepository.php
+++ src/OntoPress/Repository/FormRepository.php
@@ -4,8 +4,18 @@ namespace OntoPress\Repository;
 
 use Doctrine\ORM\EntityRepository;
 
+/**
+ * Class FormRepository
+ * 
+ *  provides needed queries for the Dashboard Form Table
+ */
 class FormRepository extends EntityRepository
 {
+    /**
+     * Count the Elements of a given table
+     * 
+     * @return number of all Forms
+     */
     public function getCount()
     {
         $query = $this->createQueryBuilder('f')
--- src/OntoPress/Repository/OntologyRepository.php
+++ src/OntoPress/Repository/OntologyRepository.php
@@ -4,8 +4,18 @@ namespace OntoPress\Repository;
 
 use Doctrine\ORM\EntityRepository;
 
+/**
+ * Class OntologyRepository
+ *
+ * provides needed queries for the Dashboard Ontology Table
+ */
 class OntologyRepository extends EntityRepository
 {
+    /**
+     * Count the Elements of a given table
+     *
+     * @return number of all Ontologies
+     */
     public function getCount()
     {
         $query = $this->createQueryBuilder('o')
@@ -13,7 +23,12 @@ class OntologyRepository extends EntityRepository
 
         return $query->getQuery()->getSingleScalarResult();
     }
-    
+
+    /**
+     * Searches the 5 most used Ontologies in the database via the number of their forms
+     *
+     * @return table with the 5 most used Ontologies
+     */
     public function getMostUsedOntologies()
     {
         $query = $this->createQueryBuilder('o')
