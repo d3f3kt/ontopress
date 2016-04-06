c6927d3e4d412c32186b48aa9d08f0647498b800
David Geistert <d3f3kt@scripts4u.net>
2016-04-06 18:05:03 +0000

    upd: DRAFT - improve twig generator

--- src/OntoPress/Service/TwigGenerator.php
+++ src/OntoPress/Service/TwigGenerator.php
@@ -38,29 +38,104 @@ class TwigGenerator
      */
     public function generate(Form $form)
     {
-        $twigCode = '';
-        $formFields = $form->getOntologyFields();
-        foreach ($formFields as $formField) {
-            $fieldName = $this->createFieldName($formField);
-            $twigCode .= '{{form_label(form.'.$fieldName.")}}\n"
-                .'{{form_errors(form.'.$fieldName.")}}\n"
-                .'{{form_widget(form.'.$fieldName.")}}\n\n";
+        $twigCode = $this->generateFormStart();
+
+        $ontologyFields = $form->getOntologyFields();
+        foreach ($ontologyFields as $ontologyField) {
+            $twigCode .= $this->generateFormField($ontologyField);
         }
+
+        $twigCode .= $this->generateFormEnd();
 
         return $twigCode;
     }
 
-    /**
-     * Helper function to generate unique name from ontology field object.
-     *
-     * @param OntologyField $formField
-     *
-     * @return string
-     */
-    private function createFieldName($formField)
+    private function generateFormField(OntologyField $ontologyField)
     {
-        return str_replace(" ", "", $formField->getDataOntology()->getOntology()->getName())
-            .'_'
-            .$this->parser->getUriFile($formField->getName());
+        switch ($ontologyField->getType()) {
+            case OntologyField::TYPE_TEXT:
+                return $this->generateTextField($ontologyField);
+            case OntologyField::TYPE_RADIO:
+                return $this->generateRadioField($ontologyField);
+            case OntologyField::TYPE_CHOICE:
+                return $this->generateChoiceField($ontologyField);
+        }
+    }
+
+    private function generateTextField(OntologyField $ontologyField)
+    {
+        //TODO
+        $fieldName = $ontologyField->getFormFieldName();
+        $twigCode .= '{{form_label(form.'.$ontologyField->getFormFieldName().")}}\n";
+        //    .'{{form_errors(form.'.$ontologyField->getFormFieldName().")}}\n";
+        $twigCode .= "<input type='text' "
+            .$this->generateFieldValue($ontologyField).' '
+            .$this->generateFieldAttributes($ontologyField)
+            ." />\n\n";
+
+        return $twigCode;
+    }
+
+    private function generateRadioField(OntologyField $ontologyField)
+    {
+        //TODO
+        $fieldName = $formField->getFormFieldName();
+        $twigCode .= '{{form_label(form.'.$fieldName.")}}\n"
+            .'{{form_errors(form.'.$fieldName.")}}\n"
+            .'{{form_widget(form.'.$fieldName.")}}\n\n";
+
+        return $twigCode;
+    }
+
+    private function generateChoiceField(OntologyField $ontologyField)
+    {
+        //TODO
+        $fieldName = $this->createFieldName($formField);
+        $twigCode .= '{{form_label(form.'.$fieldName.")}}\n"
+            .'{{form_errors(form.'.$fieldName.")}}\n"
+            .'{{form_widget(form.'.$fieldName.")}}\n\n";
+
+        return $twigCode;
+    }
+
+    private function generateFieldValue(OntologyField $ontologyField)
+    {
+        return '{{ '.$this->createFieldVarName($ontologyField).'.value is not empty ? '
+            ."'value=\"' ~ ".$this->createFieldVarName($ontologyField).' }}';
+    }
+
+    private function generateFieldAttributes(OntologyField $ontologyField)
+    {
+        return 'id="{{ '.$this->createFieldVarName($ontologyField).'.id }}" '.
+            'name="{{ '.$this->createFieldVarName($ontologyField).'.name }}"';
+    }
+
+    /**
+     * Generates HTML which contains the form head.
+     *
+     * @return string HTML
+     */
+    private function generateFormStart()
+    {
+        return '<form'
+            ." name='{{ form.vars.name }}'"
+            ." method='{{ form.vars.method }}'"
+            ." action='{{ form.vars.action }}'"
+            ." enctype='multipart/form-data'> \n\n";
+    }
+
+    /**
+     * Generates HTML which contains the closing form tag.
+     *
+     * @return string HTML
+     */
+    private function generateFormEnd()
+    {
+        return '</form>';
+    }
+
+    private function createFieldVarName(OntologyField $ontologyField)
+    {
+        return 'form.children.'.$ontologyField->getFormFieldName().'.vars';
     }
 }
