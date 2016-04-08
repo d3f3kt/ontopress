215ef7e281d8c0e888c8d1d54dd9353299b77753
Lukas Rosenkranz <lukas.rosenkranz@gmx.de>
2016-04-08 12:31:31 +0000

    upd: comments

--- src/OntoPress/Entity/DataOntology.php
+++ src/OntoPress/Entity/DataOntology.php
@@ -7,6 +7,7 @@ use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * DataOntology
+ * This PHP Object allows Doctrine to translate php Objects into an relational SQL Table via Metadata.
  *
  * @ORM\Table(name="ontopress_dataOntology")
  * @ORM\Entity()
