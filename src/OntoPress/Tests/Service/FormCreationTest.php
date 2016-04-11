4fb63b2ed5be2856b1f3290dbc2fd0c4a6d4a6d7
David Geistert <d3f3kt@scripts4u.net>
2016-04-11 19:56:11 +0000

    add: form creation test

---
+++ src/OntoPress/Tests/Service/FormCreationTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace OntoPress\Tests;
+
+use OntoPress\Library\OntoPressTestCase;
+use OntoPress\Service\DoctrineManager;
+use OntoPress\Entity\Ontology;
+use OntoPress\Entity\OntologyFile;
+use OntoPress\Tests\Entity\OntologyTest;
+use OntoPress\Tests\TestHelper;
+
+class FormCreationTest extends OntoPressTestCase
+{
+    private $formCreation;
+    private $parser;
+    private $doctrine;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->doctrine = static::getContainer()->get('doctrine');
+        $this->parser = static::getContainer()->get('ontopress.ontology_parser');
+        $this->formCreation = static::getContainer()->get('ontopress.form_creation');
+    }
+
+    public function tearDown()
+    {
+        unset($this->doctrine);
+        parent::tearDown();
+    }
+
+    public function testFormCreation()
+    {
+        $ontology = TestHelper::createTestOntology();
+        $this->parser->parsing($ontology);
+        $this->doctrine->persist($ontology);
+        $this->doctrine->flush();
+
+        $form = TestHelper::createOntologyForm($ontology);
+
+        $this->formCreation->create($form);
+
+    }
+}
--- src/OntoPress/Tests/TestHelper.php
+++ src/OntoPress/Tests/TestHelper.php
@@ -2,6 +2,11 @@
 
 namespace OntoPress\Tests;
 
+use OntoPress\Entity\Form;
+use OntoPress\Entity\Ontology;
+use OntoPress\Entity\OntologyFile;
+use OntoPress\Tests\Entity\OntologyTest;
+
 class TestHelper
 {
     public static function emulateWPUser()
@@ -17,5 +22,46 @@ class TestHelper
         );
 
         return $testUser;
+    }
+
+    public static function createTestOntology()
+    {
+        $ontologyFile1 = new OntologyFile();
+        $ontologyFile1->setFile(OntologyTest::createTmpFile('place-ontology.ttl'));
+
+        $ontologyFile2 = new OntologyFile();
+        $ontologyFile2->setFile(OntologyTest::createTmpFile('knorke.ttl'));
+        $ontology = new Ontology();
+        $ontology->setName('Test Ontology')
+            ->setAuthor('Test User')
+            ->setDate(new \DateTime())
+            ->addOntologyFile($ontologyFile1)
+            ->addOntologyFile($ontologyFile2);
+
+        $ontology->uploadFiles();
+
+        return $ontology;
+    }
+
+    public static function createOntologyForm(Ontology $ontology)
+    {
+        $form = new Form();
+        $form->setName('Test Form')
+            ->setAuthor('Test User')
+            ->setDate(new \DateTime())
+            ->setTwigCode('{{ form(form) }}')
+            ->setOntology($ontology);
+
+        foreach ($ontology->getDataOntologies() as $dataOntology) {
+            if (strpos($dataOntology->getName(), 'building')) {
+                foreach ($dataOntology->getOntologyFields() as $field) {
+                    if ($field->getLabel()) {
+                        $form->addOntologyField($field);
+                    }
+                }
+            }
+        }
+
+        return $form;
     }
 }
