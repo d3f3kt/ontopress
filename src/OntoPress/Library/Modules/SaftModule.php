f038ca01e0788e35164f73b4ce169bfd5616d6a5
David Geistert <d3f3kt@scripts4u.net>
2016-04-14 23:54:33 +0000

    upd: add saft store to dependency injection container

--- src/OntoPress/Library/Modules/SaftModule.php
+++ src/OntoPress/Library/Modules/SaftModule.php
@@ -4,8 +4,14 @@ namespace OntoPress\Library\Modules;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Saft\Addition\EasyRdf\Data\ParserEasyRdf;
+use Saft\Addition\ARC2\Store\ARC2;
 use Saft\Rdf\NodeFactoryImpl;
 use Saft\Rdf\StatementFactoryImpl;
+use Saft\Rdf\StatementIteratorFactoryImpl;
+use Saft\Sparql\Query\QueryFactoryImpl;
+use Saft\Sparql\Result\ResultFactoryImpl;
+use Saft\Store\BasicTriplePatternStore;
+use Saft\Store\Store as StoreInterface;
 
 /**
  * Module which adds Saft components to dependency injection container.
@@ -26,5 +32,38 @@ class SaftModule extends AbstractModule
         );
 
         $container->set('saft.easyrdf.parser', $parser);
+        $container->set('saft.store', $this->getStore());
+    }
+
+    /**
+     * Get Saft store according to the environment.
+     *
+     * @return StoreInterface Saft Store interface
+     */
+    private function getStore()
+    {
+        if ($this->isTestEnv()) {
+            return new BasicTriplePatternStore(
+                new NodeFactoryImpl(),
+                new StatementFactoryImpl(),
+                new QueryFactoryImpl(),
+                new StatementIteratorFactoryImpl()
+            );
+        } else {
+            return new ARC2(
+                new NodeFactoryImpl(),
+                new StatementFactoryImpl(),
+                new QueryFactoryImpl(),
+                new ResultFactoryImpl(),
+                new StatementIteratorFactoryImpl(),
+                array(
+                    'username' => DB_USER,
+                    'password' => DB_PASSWORD,
+                    'host' => DB_HOST,
+                    'database' => DB_NAME,
+                    'table-prefix' => 'ontopress_arc2_',
+                )
+            );
+        }
     }
 }
