4e27b4a21ab1b7b64655915d2b1bf641ee9d72e8
Christofer Meinecke <Christofer.M@gmx.de>
2016-04-17 15:53:28 +0000

    fix: minor spelling mistake

--- src/OntoPress/Service/DoctrineSchemaTool.php
+++ src/OntoPress/Service/DoctrineSchemaTool.php
@@ -12,17 +12,17 @@ class DoctrineSchemaTool
 
     public function __construct(EntityManager $doctrine)
     {
-        $this->metadatas = $doctrine->getMetadataFactory()->getAllMetadata();
+        $this->metaDatas = $doctrine->getMetadataFactory()->getAllMetadata();
         $this->schemaTool = new SchemaTool($doctrine);
     }
 
     public function updateSchema()
     {
-        $this->schemaTool->updateSchema($this->metadatas);
+        $this->schemaTool->updateSchema($this->metaDatas);
     }
 
     public function dropSchema()
     {
-        $this->schemaTool->dropSchema($this->metadatas);
+        $this->schemaTool->dropSchema($this->metaDatas);
     }
 }
