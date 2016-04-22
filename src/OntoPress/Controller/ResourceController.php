2264026b2d1a0c1193f1521c68de1957474f54ff
Noah <noahbwalle@googlemail.com>
2016-04-22 16:14:40 +0000

    upd: draft read triplestore from arc2

--- src/OntoPress/Controller/ResourceController.php
+++ src/OntoPress/Controller/ResourceController.php
@@ -5,7 +5,11 @@ namespace OntoPress\Controller;
 use OntoPress\Form\Resource\Type\AddResourceType;
 use OntoPress\Library\AbstractController;
 use OntoPress\Form\Resource\Type\AddResourceDetailType;
+use OntoPress\Library\Modules\SaftModule;
 use Symfony\Component\HttpFoundation\Request;
+use Saft\Rdf\NamedNode;
+use Saft\Rdf\NamedNodeImpl;
+use Saft\Rdf\StatementImpl;
 
 /**
  * Resource Controller.
@@ -93,14 +97,66 @@ class ResourceController extends AbstractController
      */
     public function showManagementAction()
     {
-        $resourceManageTable = array(
-            array('id' => 1, 'title' => 'Augustusplatz', 'author' => 'k00ni', 'date' => '20.Jan 2016'),
-            array('id' => 2, 'title' => 'Uni Campus', 'author' => 'k00ni', 'date' => '22.Jan 2016'),
-            array('id' => 3, 'title' => 'Oper Leipzig', 'author' => 'd3f3ct', 'date' => '22.Jan 2016'),
+        $tableArray = array();
+        $id = 0;
+
+        // TODO: one huge loop, until all triples were read --> statement interator?
+        $store = $this->get('saft.store');
+
+        // ## TEST ## //
+        $anyStatement = new StatementImpl(
+            new NamedNodeImpl('http://localhost/Place/'),
+            new NamedNodeImpl('http://localhost/hasProperty/'),
+            new NamedNodeImpl('http://localhost/property/')
         );
 
+        $store->addStatements($anyStatement); // TODO: ARC2 provideds statement iterator to loop through all triples?
+        $query = '
+            SELECT ?subject ?predicate ?object
+            WHERE {
+                ?subject ?predicate ?object
+            }
+        ';
+
+        dump($anyStatement);
+        $triple = $store->query($query, array());
+
+        // TODO: Author und Date sind Platzhalter zum identifizieren
+        switch($triple[1]) {
+            case 'Author':
+                $predicate = 2;
+                break;
+            case 'Date':
+                $predicate = 3;
+                break;
+            default:
+                // Exception?
+                break;
+        }
+
+        $newTitle = true;
+        $n = 0;
+        foreach($tableArray as $array) {
+            if($triple[0] == $array[0]) {
+                $tableArray[$n][$predicate] = $triple[2];
+                $newTitle = false;
+                break;
+            }
+            $n = $n+1;
+        }
+        if($newTitle == true) {
+            array_push($tableArray, array($id+1, $triple[0], '', ''));
+            $tableArray[$id][$predicate] = $triple[2];
+
+        }
+
+        /*
+         array('id' => 1, 'title' => 'Augustusplatz', 'author' => 'k00ni', 'date' => '20.Jan 2016'),
+         array('id' => 2, 'title' => 'Uni Campus', 'author' => 'k00ni', 'date' => '22.Jan 2016'),
+         array('id' => 3, 'title' => 'Oper Leipzig', 'author' => 'd3f3ct', 'date' => '22.Jan 2016'),
+         */
         return $this->render('resource/resourceManagement.html.twig', array(
-            'resourceManageTable' => $resourceManageTable
+            'resourceManageTable' => $tableArray
         ));
     }
 
