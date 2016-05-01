47319352e28552a50bfd5e87ad48ae9f89e9ac73
David Geistert <d3f3kt@scripts4u.net>
2016-05-01 18:41:25 +0000

    upd: make the doctrine validator work

---
+++ src/OntoPress/Library/ConstraintValidatorFactory.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace OntoPress\Library;
+
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidatorFactoryInterface;
+use Symfony\Component\Validator\ConstraintValidatorInterface;
+use Symfony\Component\Validator\Exception\UnexpectedTypeException;
+
+/**
+ * Uses a service container to create constraint validators.
+ *
+ * A constraint validator should be tagged as "validator.constraint_validator"
+ * in the service container and include an "alias" attribute:
+ */
+class ConstraintValidatorFactory implements ConstraintValidatorFactoryInterface
+{
+    protected $container;
+    protected $validators;
+
+    /**
+     * Constructor.
+     *
+     * @param ContainerInterface $container  The service container
+     * @param array              $validators An array of validators
+     */
+    public function __construct(ContainerInterface $container, array $validators = array())
+    {
+        $this->container = $container;
+        $this->validators = $validators;
+    }
+    /**
+     * Returns the validator for the supplied constraint.
+     *
+     * @param Constraint $constraint A constraint
+     *
+     * @return ConstraintValidatorInterface A validator for the supplied constraint
+     *
+     * @throws UnexpectedTypeException When the validator is not an instance of ConstraintValidatorInterface
+     */
+    public function getInstance(Constraint $constraint)
+    {
+        $name = $constraint->validatedBy();
+        if (!isset($this->validators[$name])) {
+            $this->validators[$name] = new $name();
+        } elseif (is_string($this->validators[$name])) {
+            $this->validators[$name] = $this->container->get($this->validators[$name]);
+        }
+        
+        if (!$this->validators[$name] instanceof ConstraintValidatorInterface) {
+            throw new UnexpectedTypeException($this->validators[$name], 'Symfony\Component\Validator\ConstraintValidatorInterface');
+        }
+
+        return $this->validators[$name];
+    }
+}
--- src/OntoPress/Library/Modules/ValidatorModule.php
+++ src/OntoPress/Library/Modules/ValidatorModule.php
@@ -4,6 +4,7 @@ namespace OntoPress\Library\Modules;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Validator\Validation;
+use OntoPress\Library\ConstraintValidatorFactory;
 
 /**
  * Module which adds the Symfony Validator component to the dependency injection container.
@@ -17,10 +18,21 @@ class ValidatorModule extends AbstractModule
      */
     public function process(ContainerBuilder $container)
     {
+        $validators = array();
+        foreach ($container->findTaggedServiceIds('validator.constraint_validator') as $id => $tag) {
+            $validators[$id] = $id;
+        }
+
+        $validatorConstraintFactory = new ConstraintValidatorFactory(
+            $container,
+            $validators
+        );
+
         $validator = Validation::createValidatorBuilder()
             ->enableAnnotationMapping()
             ->setTranslator($container->get('symfony.translator'))
             ->setTranslationDomain('validators')
+            ->setConstraintValidatorFactory($validatorConstraintFactory)
             ->getValidator();
 
         $container->set('symfony.validator', $validator);
