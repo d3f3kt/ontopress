3a2539fd83b7b2f32d7212232cd6e93d19369f7f
Christofer Meinecke <Christofer.M@gmx.de>
2016-05-03 15:28:24 +0000

    upd: select forms by ontology draft and some minor changes

--- src/OntoPress/Tests/TestHelper.php
+++ src/OntoPress/Tests/TestHelper.php
@@ -65,16 +65,11 @@ class TestHelper
         return $dataOntology;
     }
 
-    public static function createOntologyField(DataOntology $dataOntology = null, Restriction $restriction = null)
+    public static function createOntologyField(DataOntology $dataOntology, Restriction $restriction = null)
     {
         if (!$restriction) {
             $restriction = new Restriction();
             $restriction->setName('Test Restriction');
-        }
-
-        if (!$dataOntology) {
-            $dataOntology = new DataOntology();
-            $dataOntology->setName('Test DataOntology');
         }
 
         $ontologyField = new OntologyField();
@@ -85,6 +80,7 @@ class TestHelper
             ->setLabel('Test Label')
             ->setMandatory(true)
             ->setDataOntology($dataOntology)
+            ->setRegex('Test Regex')
             ->setPossessed(true)
             ->addRestriction($restriction);
 
@@ -94,16 +90,8 @@ class TestHelper
         return $ontologyField;
     }
 
-    public static function createOntologyForm(Ontology $ontology = null, OntologyField $ontologyField = null)
+    public static function createOntologyForm(Ontology $ontology, OntologyField $ontologyField = null)
     {
-        if (!$ontology) {
-            $ontology = new Ontology();
-            $ontology
-                ->setName('Test Ontology '. uniqid())
-                ->setAuthor('Test User')
-                ->setDate(new \DateTime());
-        }
-
         $form = new Form();
         $form
             ->setName('Test Form')
