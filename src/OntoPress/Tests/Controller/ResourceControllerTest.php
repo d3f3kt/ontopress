d739b0104d1fa0cc408290a19c9127c3fd954a92
Enno <e.jertschat@gmail.com>
2016-05-08 20:04:34 +0000

    upd: ResourceControllerTest

--- src/OntoPress/Tests/Controller/ResourceControllerTest.php
+++ src/OntoPress/Tests/Controller/ResourceControllerTest.php
@@ -53,7 +53,10 @@ class ResourceControllerTest extends OntoPressTestCase
         static::getContainer()->get('doctrine')->persist($formEntity);
         static::getContainer()->get('doctrine')->flush();
 
+        // test  without valid form
         $this->assertContains("addResourceType", $this->resourceController->showAddAction(new Request()));
+
+        // test with valid form
         $resourceOutputValidForm = $this->resourceController->showAddAction(
             new Request(
                 array(),
@@ -67,7 +70,6 @@ class ResourceControllerTest extends OntoPressTestCase
                 array('REQUEST_METHOD' => 'POST')
             )
         );
-
         $this->assertContains("window.location", $resourceOutputValidForm);
     }
 
@@ -78,25 +80,43 @@ class ResourceControllerTest extends OntoPressTestCase
     {
         $ontologyEntity = TestHelper::createTestOntology();
         $formEntity = TestHelper::createOntologyForm($ontologyEntity);
+        static::getContainer()->get('doctrine')->persist($ontologyEntity);
+        static::getContainer()->get('doctrine')->persist($formEntity);
+        static::getContainer()->get('doctrine')->flush();
+        Functions::when('wp_get_current_user')->alias(array('OntoPress\Tests\TestHelper', 'emulateWPUser'));
 
-
+        // test without ID
         $resourceNoID = $this->resourceController->showAddDetailsAction(new Request());
+        $this->assertContains("window.location", $resourceNoID);
+
+        // test with Id, but no formEntity
         $resourceNoEntity = $this->resourceController->showAddDetailsAction(new Request(array(
             'formId' => 2000
         )));
+        $this->assertContains("Formular nicht gefunden!", $resourceNoEntity);
+
+        // valid resource, but no valid form
         $resourceValid = $this->resourceController->showAddDetailsAction(new Request(array(
-            'formId' => 2,
+            'formId' => 1,
             'formEntity' => $formEntity
         )));
-        /*
-        $resourceValidForm = $this->resourceController->showAddDetailsAction(new Request(
-
-        ));
-        $this->assertContains("window.location", $resourceValidForm);
-        */
-        $this->assertContains("window.location", $resourceNoID);
-        $this->assertContains("Formular nicht gefunden!", $resourceNoEntity);
         $this->assertContains("Ressourcen Hinzufügen", $resourceValid);
+
+        // valid resource, valid form
+        $resourceValidForm = $this->resourceController->showAddAction(
+            new Request(
+                array(),
+                array('addResourceType' => array(
+                    'form' => '1',
+                    'submit' => ''
+                )),
+                array(),
+                array(),
+                array(),
+                array('REQUEST_METHOD' => 'POST')
+            )
+        );
+        $this->assertContains("window.location", $resourceValidForm);
     }
 
     /**
@@ -104,6 +124,30 @@ class ResourceControllerTest extends OntoPressTestCase
      */
     public function testShowManagementAction()
     {
+        // 's' empty, without graph, with order request
+        $this->resourceController->showManagementAction(
+            new Request(
+                array('orderBy' => 'Id')
+            )
+        );
+
+        // 's' not empty, no graph
+        $this->resourceController->showManagementAction(
+            new Request(
+                array('s' => 'searchString')
+            )
+        );
+
+        // 's' not empty, with graph
+        $this->resourceController->showManagementAction(
+            new Request(
+                array(
+                    's' => 'searchString',
+                    'graph' => 'Test:Uri')
+            )
+        );
+
+        // 's' empty, with graph
         $resourceOutput = $this->resourceController->showManagementAction(
             new Request(
                 array(
@@ -119,13 +163,98 @@ class ResourceControllerTest extends OntoPressTestCase
      */
     public function testShowDeleteAction()
     {
-        $resourceOutput = $this->resourceController->showDeleteAction(
+        $ontologyEntity = TestHelper::createTestOntology();
+        $formEntity = TestHelper::createOntologyForm($ontologyEntity);
+        static::getContainer()->get('doctrine')->persist($ontologyEntity);
+        static::getContainer()->get('doctrine')->persist($formEntity);
+        static::getContainer()->get('doctrine')->flush();
+
+        // uri, no valid form
+        $result1 = $this->resourceController->showDeleteAction(
             new Request(
                 array(
                     'uri' => 'Test:Uri'
                 )
             )
         );
-        $this->assertContains("Ressource löschen", $resourceOutput);
+        $this->assertContains("Ressource Löschen", $result1);
+
+        /*
+        // uri, valid form
+        $result2 = $this->resourceController->showDeleteAction(
+            new Request(
+                array(),
+                array(
+                    'resourceDeleteType' => array(
+                        'submit' => ''
+                )),
+                array(
+                    'uri' => 'Test:UriDelete'
+                ),
+                array(),
+                array(),
+                array('REQUEST_METHOD' => 'POST')
+            )
+        );
+        $this->assertContains('window.location', $result2);
+        */
+        // no uri, no valid form
+        $result3 = $this->resourceController->showDeleteAction(
+            new Request()
+        );
+        $this->assertContains("Ressource Löschen", $result3);
+
+        // no uri, valid form
+        /*
+        $result4 = $this->resourceController->showDeleteAction(
+            new Request(
+                array(),
+                array(
+                    'resourceDeleteType' => array(
+                        'submit' => ''
+                    )),
+                array(
+                    'resources' => ''
+                ),
+                array(),
+                array(),
+                array('REQUEST_METHOD' => 'POST')
+            )
+        );
+        $this->assertContains("window.location", $result4);
+        */
+    }
+
+    public function testShowEditAction()
+    {
+        $ontologyEntity = TestHelper::createTestOntology();
+        $formEntity = TestHelper::createOntologyForm($ontologyEntity);
+        static::getContainer()->get('doctrine')->persist($ontologyEntity);
+        static::getContainer()->get('doctrine')->persist($formEntity);
+        static::getContainer()->get('doctrine')->flush();
+        /*
+        // uri, no Respository form
+        $result4 = $this->resourceController->showDeleteAction(
+            new Request(
+                array(),
+                array(
+                    '' => array(
+                        'submit' => ''
+                    )),
+                array(
+                    'uri' => 'Test:UriDelete'
+                ),
+                array(),
+                array(),
+                array('REQUEST_METHOD' => 'POST')
+            )
+        );
+        $this->assertContains("window.location", $result4);
+        */
+        // uri, repository form, valid form
+
+        // uri, repository form, no valid form
+
+        // no uri
     }
 }
